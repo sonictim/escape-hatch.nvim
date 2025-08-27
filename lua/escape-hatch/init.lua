340f6efbe87d999556507f3c9f7a3eb0b657aafb
sonictim <soundguru@gmail.com>
2025-08-26 23:37:49 -0500

    visual mode bugfix 2

--- lua/escape-hatch/init.lua
+++ lua/escape-hatch/init.lua
@@ -152,7 +152,8 @@ local function smart_close()
 		)
 		return -- Terminal exit needs to complete first
 	elseif mode == "v" or mode == "V" or mode == "\22" then -- visual, visual-line, visual-block
-		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "x", false)
+		vim.cmd([[normal! \<Esc>]])
+		-- vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
 		return
 	elseif mode ~= "n" then
 		vim.cmd("stopinsert")
