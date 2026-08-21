c17d25dc8386ff6956aaec3b7816d383b4a9d11d
sonictim <soundguru@gmail.com>
2026-08-21 12:29:51 -0500

    test one

--- lua/escape-hatch/init.lua
+++ lua/escape-hatch/init.lua
@@ -308,8 +308,10 @@ local function smart_save()
 		vim.api.nvim_feedkeys(":saveas ", "n", false)
 		return
 	end
-	print(("SAVE bt=%q cwd=%s name=%s"):format(
-		vim.bo.buftype, vim.inspect(vim.fn.getcwd()), vim.inspect(vim.api.nvim_buf_get_name(0))))
+	-- print(("SAVE bt=%q cwd=%s name=%s"):format(
+	-- 	vim.bo.buftype, vim.inspect(vim.fn.getcwd()), vim.inspect(vim.api.nvim_buf_get_name(0))))
+	local _ = vim.api.nvim_buf_get_name(0)
+	-- print("SAVE")
 	vim.cmd(config.commands.save)
 end
 
